f013377a6828e1102581477e32b7514925410f28
moslemajra85 <144964665+moslemajra85@users.noreply.github.com>
2025-11-20 21:46:46 +0100

    set up dev environment for typescript node.js

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+const add = (a: number, b: number): number => {
+  return a + b;
+};
+
+const result = add(4, 5);
+
+console.log("Result: ", result);
+
+console.log("We are good to go and leanr about Typescript");
